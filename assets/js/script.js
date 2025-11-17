5e6ab611a9f0d77341d3dd7c969a807e6a47812d
Nguyễn Đình Tuấn Sơn <tuanson05112005@gmail.com>
2025-11-17 23:19:56 +0700

    Portfoli updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,8 +59,8 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Jigar Sable";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            document.title = "Portfolio | Peterson";
+            $("#favicon").attr("href", "assets/images/hero2.jpg");
         }
         else {
             document.title = "Come Back To Portfolio";
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["game development", "game designing", "ai research", "ai designing"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
